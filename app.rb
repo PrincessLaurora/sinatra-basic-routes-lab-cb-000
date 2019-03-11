03fd8a32d5b234b0fbc7d172904a055697674f87
Laura Felce <crepu83@yahoo.it>
2019-03-11 16:12:55 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -4,13 +4,14 @@ class App < Sinatra::Base
 
   get '/name' do
     "My name is Laurora"
+  end  
 
   get '/hometown' do
      "My hometown is Quartucciu city"
+  end 
 
   get '/favorite-song' do
      "My favorite song is Trololo"
   end
-  end
-  end
+  
 end
